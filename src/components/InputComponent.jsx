9c5ea1b40b9d3f056aab7cfbda98b5e4299081a9
Moch Diki Widianto <dikiwidia@live.com>
2024-11-05 13:33:42 +0700

    update script and set limit to 100

--- src/components/InputComponent.jsx
+++ src/components/InputComponent.jsx
@@ -1,15 +1,22 @@
 const InputComponent = (props) => {
-  const { label, name, type = "text", placeholder = null } = props;
+  const {
+    label,
+    name,
+    type = "text",
+    require = false,
+    placeholder = null,
+  } = props;
   return (
     <div className="flex flex-col items-start justify-center w-full gap-1">
       <label htmlFor={name} className="w-full font-bold">
-        {label}:
+        {label} {require && <span className="text-red-500">*</span>} :
       </label>
       <input
         className="px-2 py-2 border border-sky-500 outline-sky-500 rounded-md w-full"
         type={type}
         name={name}
         placeholder={placeholder || label}
+        required={require}
       />
     </div>
   );
--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import InputComponent from "../components/InputComponent";
 import TimesComponent from "../components/TimesComponent";
-import { useEffect } from "react";
 
 const MainPage = () => {
   const [length, setLength] = useState(10);
@@ -9,16 +8,36 @@ const MainPage = () => {
 
   const submitAction = (e) => {
     e.preventDefault();
-    setLength(e.target.length.value);
-    setLimit(e.target.limit.value);
+    const length = e.target.length.value;
+    const limit = e.target.limit.value;
+    if (length < 1 || length > 100) {
+      alert("Panjang harus antara 1 sampai 100");
+      return;
+    }
+    if (limit < 1 || limit > 100) {
+      alert("Batas akhir harus antara 1 sampai 100");
+      return;
+    }
+    setLength(length);
+    setLimit(limit);
   };
 
   return (
     <div className="flex items-center justify-center flex-col py-4">
       <h1 className="text-3xl font-bold mb-2">Tabel Perkalian</h1>
       <form className="w-full xl:w-1/4 px-2 py-2 mx-2" onSubmit={submitAction}>
-        <InputComponent label="Perkalian Sampai" name="length" type="number" />
-        <InputComponent label="Batas Akhir" name="limit" type="number" />
+        <InputComponent
+          label="Perkalian Sampai"
+          name="length"
+          type="number"
+          require={true}
+        />
+        <InputComponent
+          label="Batas Akhir"
+          name="limit"
+          type="number"
+          require={true}
+        />
         <button
           type="submit"
           className="px-2 py-2 mt-2 bg-sky-500 w-full rounded text-white"
